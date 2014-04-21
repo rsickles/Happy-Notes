eb288d739b5f277307840f1c683e8b1bd92895a3
Ryan Sickles <rsickles@andrew.cmu.edu>
2014-04-21 17:29:34 -0400

    Initial Commit

---
+++ HappyNotes/RSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RSAppDelegate.h
+//  HappyNotes
+//
+//  Created by Ryan Sickles on 4/21/14.
+//  Copyright (c) 2014 sickles.ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HappyNotes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HappyNotes
+//
+//  Created by Ryan Sickles on 4/21/14.
+//  Copyright (c) 2014 sickles.ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RSAppDelegate class]));
+    }
+}
